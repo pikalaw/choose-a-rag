138e2f6e51355857f4e6985e057156f715a45d26
Lawrence Tsang <pikalaw@gmail.com>
2023-11-28 10:24:13 -0500

    Fixed Palm

--- api/llms/genaix.py
+++ api/llms/genaix.py
@@ -1,9 +1,17 @@
 import google.ai.generativelanguage as genai
+from google.api_core import client_options as client_options_lib
 from typing import Iterable
 
 
+_DEFAULT_API_ENDPOINT = "autopush-generativelanguage.sandbox.googleapis.com"
+
+
 def list_models() -> Iterable[genai.Model]:
-  service = genai.ModelServiceClient()
+  service = genai.ModelServiceClient(
+      client_options=client_options_lib.ClientOptions(
+          api_endpoint=_DEFAULT_API_ENDPOINT
+      ),
+  )
   models = service.list_models(
       request=genai.ListModelsRequest()
   )
@@ -11,7 +19,11 @@ def list_models() -> Iterable[genai.Model]:
 
 
 def generate_text(model: str, prompt: str) -> str:
-  service = genai.TextServiceClient()
+  service = genai.TextServiceClient(
+      client_options=client_options_lib.ClientOptions(
+          api_endpoint=_DEFAULT_API_ENDPOINT
+      ),
+  )
   response = service.generate_text(
       request=genai.GenerateTextRequest(
           model=model,
